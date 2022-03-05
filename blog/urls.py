1c7d777396bd1e5c71e5bc18aed48f7e955ed0bd
Fabio Cordeiro de Araujo <fabio.araujo@serpro.gov.br>
2022-03-05 10:34:22 -0300

    TASK 0503221033 - First commit

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns 
+from django.conf.urls.static import static
+from django.conf import settings
+
+from blog import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += i18n_patterns(
+    path("", views.home, name="home"),
+    path("i18n/", include("django.conf.urls.i18n")),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
